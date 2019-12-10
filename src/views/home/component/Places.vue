305216870cdec68772e0f3b88ca05cba38948941
DoubleLTT <1016875686@qq.com>
2019-12-10 18:05:34 +0800

    pictures

--- src/views/home/component/Places.vue
+++ src/views/home/component/Places.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="" style="padding: 40px;">
     <Tabs :animated="false" type="card">
-      <TabPane label="标签一" icon="md-home">
+      <TabPane label="景点" icon="md-home">
         <Row>
-          <Col span="4">
-            <div style="height: 400px; background-color: #f7f7f7;margin-right: 20px">
-              推荐
+          <i-col span="4">
+            <div style="height: 400px; background-color: #f7f7f7;margin-right: 20px;padding:10px 10px;">
+              <p style="margin-top: 10px;">级别：
+                <i-select :model.sync="model1" style="width:60%;" size="small" placeholder="所有">
+                  <i-option v-for="item in star" :value="item.value">{{ item.label }}</i-option>
+                </i-select>
+              </p>
+              <p style="margin-top: 10px;">区域：
+                <i-select :model.sync="model1" style="width:60%;" size="small" placeholder="区(市)县">
+                  <i-option v-for="item in area" :value="item.value">{{ item.label }}</i-option>
+                </i-select>
+              </p>
+              <p style="margin-top: 10px;">搜索</p>
             </div>
-          </Col>
-          <Col span="20" >
-            <div style="height: 400px; background-color: #f7f7f7; margin: 5px">
-              <Card style="width:320px">
-                <div style="text-align:center">
-                  <img src="../../../../static/places/dujiangyan.jpg">
-                  <h3>A high quality UI Toolkit based on Vue.js</h3>
-                </div>
-              </Card>
+          </i-col>
+          <i-col span="20" >
+            <div style="height: 100%; background-color: #f7f7f7;padding-top:20px;">
+              <Row type="flex" justify="space-around">
+                <i-col span="7" v-for="item in place_json">
+                  <div style="width:100%;border: 1px solid rgb(153, 153, 153);margin-bottom: 20px;">
+                    <div style="width:100%;overflow: hidden;"><img :src="item.url" class="image"></div>
+                    <p style="margin:0 10px;font-weight:bold;font-size:18px;font-family: 'Poppins', sans-serif;">{{item.name}}<span v-for="n in parseInt(item.star)" style="display: inline-block;float: right;"><Icon type="md-star" color="#f15b5c" size="20"/></span></p>
+                    <p style="margin:10px"><Icon type="ios-pin" />{{item.address}} <span style="display: inline-block;float: right;">{{item.area}}</span></p>
+                  </div>
+                </i-col>
+              </Row>
             </div>
-          </Col>
+            <div style="text-align: center;margin-top: 10px;">
+              <Page :total="total" :page-size="6" :current="1" @on-change="getPic" show-total></Page>
+            </div>
+          </i-col>
         </Row>
       </TabPane>
-      <TabPane label="标签二" icon="md-home">标签二的内容</TabPane>
-      <TabPane label="标签三" icon="md-home">标签三的内容</TabPane>
+      <TabPane label="美食" icon="md-home">标签二的内容</TabPane>
+      <TabPane label="路线" icon="md-home">标签三的内容</TabPane>
     </Tabs>
   </div>
 </template>
@@ -54,19 +70,49 @@
           // },
         ],
         error_msg:'',
-        page:0  //控制翻页按钮是否可点击
+        page:0,  //控制翻页按钮是否可点击
+        total:0,
+        star: [
+          {
+            value: '5A',
+            label: '5A'
+          },{
+            value: '4A',
+            label: '4A'
+          },{
+            value: '3A',
+            label: '3A'
+          },{
+            value: '2A',
+            label: '2A'
+          },
+        ],
+        area: [
+          {
+            value: 'dujiangyan',
+            label: '都江堰'
+          },{
+            value: 'jinjiang',
+            label: '锦江'
+          },{
+            value: 'qinyang',
+            label: '青羊'
+          },
+        ],
+        model1: ''
 			}
 		},
     mounted (){
-      this.getPic(0); //0：加载热度最高的前3条地点  3：加载第4~6条
+      this.getPic(1); //1：前6条地点  2：加载第6~12条
     },
     methods :{
       getPic (page){
-        this.page=page;
+        this.total=30;
         getPics(page).then(response=>{
               let res=response.data;
               if(res.valid){
                 this.place_json=[];
+                // res.pics.star = res.pics.star.substring(0,1);
                 this.place_json=res.pics
               }
         })
@@ -82,14 +128,19 @@
   ul{
     list-style-type: none;
   }
-  .ivu-tabs-bar{
-    margin-bottom:0!important;
-  }
   .ivu-tabs-bar .ivu-tabs-tab{
     margin-right: 10px!important;
   }
   .demo-tabs-style2 > .ivu-tabs.ivu-tabs-card > .ivu-tabs-bar .ivu-tabs-tab{
     border-radius: 0;
     background: #fff;
+  }
+  .image{
+    width:100%;
+    height:auto;
+    transition: .5s;
+  }
+  .image:hover{
+    transform: scale(1.1);
   }
 </style>
