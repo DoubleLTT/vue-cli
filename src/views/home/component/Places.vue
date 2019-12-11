024865faa82115ee82c7df1c3d8b803d27e5279d
DoubleLTT <1016875686@qq.com>
2019-12-11 23:13:25 +0800

    UI

--- src/views/home/component/Places.vue
+++ src/views/home/component/Places.vue
@@ -5,21 +5,28 @@
         <Row>
           <i-col span="4">
             <div style="height: 400px; background-color: #f7f7f7;margin-right: 20px;padding:10px 10px;">
-              <p style="margin-top: 10px;">级别：
-                <i-select :model.sync="model1" style="width:60%;" size="small" placeholder="所有" @on-change="classSearch">
+              <p style="margin-top: 15px;">级别：
+                <i-select :model.sync="model1" style="width:70%;" size="small" placeholder="所有" @on-change="classSearch">
                   <i-option v-for="(item,index) in star" :value="item.value" :key="index">{{ item.label }}</i-option>
                 </i-select>
               </p>
-              <p style="margin-top: 10px;">区域：
-                <i-select :model.sync="model1" style="width:60%;" size="small" placeholder="区(市)县" @on-change="areaSearch">
+              <p style="margin-top: 15px;">区域：
+                <i-select :model.sync="model1" style="width:70%;" size="small" placeholder="区(市)县" @on-change="areaSearch">
                   <i-option v-for="(item,index) in area" :value="item.value" :key="index">{{ item.label }}</i-option>
                 </i-select>
               </p>
-              <p style="margin-top: 10px;">搜索</p>
+              <p style="margin-top: 15px;">
+                <Input v-model="searchList.name" size="small" suffix="ios-search" placeholder="请输入名称" style="width: auto" clearable @on-blur="nameSearch"/>
+              </p>
+              <div style="margin-top:30px;border-top:1px dashed rgb(153, 153, 153);padding:10px 5px 0; ">
+                <h4 style="font-weight: bold;margin-bottom: 10px;">热门景点</h4>
+                <p>青城山<Divider type="vertical" />都江堰<Divider type="vertical" />锦里<Divider type="vertical" />锦里<Divider type="vertical" />都江堰<Divider type="vertical" />青城山</p>
+              </div>
             </div>
           </i-col>
           <i-col span="20" >
             <div style="height: 100%; background-color: #f7f7f7;padding-top:20px;">
+              <p style="height:100%;text-align:center;padding-bottom: 5px;">{{this.error_msg}}</p>
               <Row type="flex" justify="space-around">
                 <i-col span="7" v-for="(item,index) in place_json" :key="index">
                   <div style="width:100%;border: 1px solid rgb(153, 153, 153);margin-bottom: 20px;">
@@ -36,8 +43,8 @@
           </i-col>
         </Row>
       </TabPane>
-      <TabPane label="美食" icon="md-home">标签二的内容</TabPane>
-      <TabPane label="路线" icon="md-home">标签三的内容</TabPane>
+      <TabPane label="美食" icon="ios-ice-cream">标签二的内容</TabPane>
+      <TabPane label="路线" icon="md-paw">标签三的内容</TabPane>
     </Tabs>
   </div>
 </template>
@@ -81,6 +88,27 @@
           },{
             value: '青羊',
             label: '青羊区'
+          },{
+            value: '武侯',
+            label: '武侯区'
+          },{
+            value: '金牛',
+            label: '金牛区'
+          },{
+            value: '成华',
+            label: '成华区'
+          },{
+            value: '双流',
+            label: '双流区'
+          },{
+            value: '天府新区',
+            label: '天府新区'
+          },{
+            value: '龙泉驿',
+            label: '龙泉驿区'
+          },{
+            value: '大邑',
+            label: '大邑县'
           },
         ],
         model1: '',
@@ -104,6 +132,7 @@
                 this.place_json=res.pics;
               }else{
                 this.place_json=[];
+                this.error_msg="没有数据";
               }
         })
        },
@@ -116,6 +145,9 @@
         console.log(value);
         this.searchList.area =value;
         this.getPic(1);
+      },
+      nameSearch(){
+        console.log(this.searchList.name)
       },
       detail (id){
         this.$router.push({name:'PlaceDetail',path:'/PlaceDetail',query:{id:id} })
